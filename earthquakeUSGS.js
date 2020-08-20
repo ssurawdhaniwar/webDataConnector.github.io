5327037597a5932f51e990e998c770c6b223ffb4
ssurawdhaniwar <61799612+ssurawdhaniwar@users.noreply.github.com>
2020-08-20 15:36:21 +0530

    Update earthquakeUSGS.js

--- earthquakeUSGS.js
+++ earthquakeUSGS.js
@@ -5,22 +5,17 @@
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
         var cols = [{
-            id: "id",
+            id: "date",
+			alias: "date",
+            dataType: tableau.dataTypeEnum.date
+        }, {
+            id: "state",
+            alias: "state",
             dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "mag",
-            alias: "magnitude",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "title",
-            alias: "title",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "location",
-            dataType: tableau.dataTypeEnum.geometry
         }];
 
-        var tableSchema = {
+        
+		var tableSchema = {
             id: "earthquakeFeed",
             alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
             columns: cols
@@ -31,18 +26,17 @@
 
     // Download the data
     myConnector.getData = function(table, doneCallback) {
-        $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
+        $.getJSON("https://api.covidtracking.com/v1/states/daily.json", function(resp) {
             var feat = resp.features,
                 tableData = [];
 
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-                    "id": feat[i].id,
-                    "mag": feat[i].properties.mag,
-                    "title": feat[i].properties.title,
-                    "location": feat[i].geometry
-                });
+                    "date": feat[i].properties.date,
+                    "state": feat[i].properties.state
+                    		
+	              });
             }
 
             table.appendRows(tableData);
@@ -55,7 +49,7 @@
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
         $("#submitButton").click(function() {
-            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+            tableau.connectionName = "COVID Tracking Feed"; // This will be the data source name in Tableau
             tableau.submit(); // This sends the connector object to Tableau
         });
     });
