2a60fa6d78af9eb675d214bde584100cea99f188
ssurawdhaniwar <61799612+ssurawdhaniwar@users.noreply.github.com>
2020-08-21 19:21:06 +0530

    Update earthquakeUSGS.js

--- earthquakeUSGS.js
+++ earthquakeUSGS.js
@@ -1,62 +1,69 @@
+
 (function() {
-    // Create the connector object
+    //1: *************************
+    // Criação do objeto da conexão
     var myConnector = tableau.makeConnector();
 
-    // Define the schema
+    //2: *************************
+    // Definição do esquema
     myConnector.getSchema = function(schemaCallback) {
-        var cols = [{
-            id: "id",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "mag",
-            alias: "magnitude",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "title",
-            alias: "title",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "location",
-            dataType: tableau.dataTypeEnum.geometry
-        }];
+        var cols = [
+            {id:"date", alias: "date", dataType: tableau.dataTypeEnum.string},
+            {id:"state", alias: "state", dataType: tableau.dataTypeEnum.string}
+        ];
 
+        //Definicao da Tabela
         var tableSchema = {
-            id: "earthquakeFeed",
-            alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+            id: "Covid_19",
+            alias: "Casos-19",
             columns: cols
         };
 
         schemaCallback([tableSchema]);
     };
 
-    // Download the data
-    myConnector.getData = function(table, doneCallback) {
-        $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-            var feat = resp.features,
-                tableData = [];
+    //3: *************************
+    // Obtencão dos dados
+    myConnector.getData = function(table, doneCallback) {  
+        function getAllData(url) {  
+          $.getJSON(url, function(resp) {  
+            var feat = resp.results,  
+              next = resp.next;  
+      
+            // Interagindo no objeto JSON  
+      
+            for (var i = 1, len = feat.length; i < len; i++) {  
+              tableData.push({  
+                "date": feat[i].date,
+                "state": feat[i].state 
+              });  
+            }  
+            // Interagindo entre todas as paginas, para isso fazemos a variavel next = resp.next; para pegar proxima pagina com dados , se nao encontrar mais dados vai ser null
+            // https://community.tableau.com/thread/335501 contribuição da Keshia Rose
+            if (next !== null) {  
+              getAllData(next);  
+            } else {  
+              table.appendRows(tableData);  
+              doneCallback();  
+            }  
+          });  
+        }  
+      
+        var tableData = [];  
+        var url = "https://covidtracking.com/api/v1/states/daily.json";  
+        getAllData(url);  
+         
+      }; 
 
-            // Iterate over the JSON object
-            for (var i = 0, len = feat.length; i < len; i++) {
-                tableData.push({
-                    "id": feat[i].id,
-                    "mag": feat[i].properties.mag,
-                    "title": feat[i].properties.title,
-                    "location": feat[i].geometry
-                });
-            }
-
-            table.appendRows(tableData);
-            doneCallback();
-        });
-    };
-
+    //4: *************************
     tableau.registerConnector(myConnector);
 
-    // Create event listeners for when the user submits the form
+    //5: *************************
+    // Criação do evento que fica escutando quando o usuário clica no botão da pagina HTML
     $(document).ready(function() {
         $("#submitButton").click(function() {
-            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
-            tableau.submit(); // This sends the connector object to Tableau
+            tableau.connectionName = "Casos do coronavírus por município por dia"; // Este texto vai ser o nome na fonte de dados no Tableau
+            tableau.submit(); // Este comando envia o objeto conexão criado no inicio para o Tableau
         });
     });
 })();
