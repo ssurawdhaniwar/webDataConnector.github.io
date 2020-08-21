456f2d969071d77087e7025ff79eb5d3c466a9cd
ssurawdhaniwar <61799612+ssurawdhaniwar@users.noreply.github.com>
2020-08-21 15:51:57 +0530

    Update earthquakeUSGS.js

--- earthquakeUSGS.js
+++ earthquakeUSGS.js
@@ -1,3 +1,11 @@
+/*
+ * Coleta dados da Secretarias de Saúde das Unidades Federativas, dados tratados por Álvaro Justen e colaboradores/Brasil.IO
+ * Para mais detalhes acesse aqui https://brasil.io/api/dataset/covid19 para mais detalhes da documentação da API
+ * Mais detalhes sobre o Tableau Web Data Connector acesse aqui https://tableau.github.io/webdataconnector/docs/
+ * 
+ * @Autor Juracy Americo <jamerico@tableau.com>
+ */
+
 
 (function() {
     //1: *************************
@@ -8,8 +16,17 @@
     // Definição do esquema
     myConnector.getSchema = function(schemaCallback) {
         var cols = [
-            {id:"date", alias: "date", dataType: tableau.dataTypeEnum.string},
-            {id:"state", alias: "state", dataType: tableau.dataTypeEnum.string}
+            {id:"city", alias: "Cidade", dataType: tableau.dataTypeEnum.string},
+            {id:"city_ibge_code", alias: "Codigo IBGE", dataType: tableau.dataTypeEnum.string},
+            {id:"confirmed", alias: "Confirmados", dataType: tableau.dataTypeEnum.float},
+            {id:"confirmed_per_100k_inhabitants", alias: "Confirmados por 100k hab.", dataType: tableau.dataTypeEnum.int},
+            {id:"date", alias: "Data", dataType: tableau.dataTypeEnum.date},
+            {id:"death_rate", alias: "Mortes/confirmados", dataType: tableau.dataTypeEnum.float},
+            {id:"deaths", alias: "Fatalidades", dataType: tableau.dataTypeEnum.float},
+            {id:"estimated_population_2019", alias: "População estimada 2019", dataType: tableau.dataTypeEnum.int},
+            {id:"is_last", alias: "É a última atualização?", dataType: tableau.dataTypeEnum.string},
+            {id:"place_type", alias: "Tipo de local", dataType: tableau.dataTypeEnum.string},
+            {id:"state", alias: "Estado", dataType: tableau.dataTypeEnum.string}
         ];
 
         //Definicao da Tabela
@@ -34,7 +51,16 @@
       
             for (var i = 1, len = feat.length; i < len; i++) {  
               tableData.push({  
+                "city": feat[i].city,
+                "city_ibge_code": feat[i].city_ibge_code,
+                "confirmed": feat[i].confirmed,
+                "confirmed_per_100k_inhabitants": feat[i].confirmed_per_100k_inhabitants,
                 "date": feat[i].date,
+                "death_rate": feat[i].death_rate,
+                "deaths": feat[i].deaths,
+                "estimated_population_2019": feat[i].estimated_population_2019,
+                "is_last": feat[i].is_last,
+                "place_type": feat[i].place_type,
                 "state": feat[i].state 
               });  
             }  
@@ -52,7 +78,7 @@
         var tableData = [];  
         var url = "https://brasil.io/api/dataset/covid19/caso/data?page=1";  
         getAllData(url);  
-         
+          
       }; 
 
     //4: *************************
