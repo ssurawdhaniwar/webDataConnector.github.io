c2d9dcc24fe55a9fd4e310457ec5b319a280a329
ssurawdhaniwar <61799612+ssurawdhaniwar@users.noreply.github.com>
2020-08-21 11:02:54 +0530

    Update earthquakeUSGS.js

--- earthquakeUSGS.js
+++ earthquakeUSGS.js
@@ -27,15 +27,14 @@
     // Download the data
     myConnector.getData = function(table, doneCallback) {
         $.getJSON("https://api.covidtracking.com/v1/states/daily.json", function(resp) {
-		
-            var feat = resp.features,
-			
+		var feat = resp.features,
+			len = feat.length
                 tableData = [];
 
             // Iterate over the JSON object
-            for (var i = 0, len = feat.length; i < len; i++) {
+            for (var i = 0; i < len; i++) {
                 tableData.push({
-                    "date": feat[i]["Month"]["Year"],
+                    "date": feat[i].properties.date,
                     "state": feat[i].properties.state
                     		
 	              });
