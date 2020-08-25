98abd0e05033c2fd188111d3c5706b2eb8331e27
ssurawdhaniwar <61799612+ssurawdhaniwar@users.noreply.github.com>
2020-08-25 13:15:43 +0530

    Update earthquakeUSGS.js

--- earthquakeUSGS.js
+++ earthquakeUSGS.js
@@ -5,31 +5,43 @@
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
         var cols = [{
-            id: "date",
-			alias: "date",
+            id: "id",
             dataType: tableau.dataTypeEnum.string
         }, {
-            id: "state",
-            alias: "state",
+            id: "mag",
+            alias: "magnitude",
+            dataType: tableau.dataTypeEnum.float
+        }, {
+            id: "title",
+            alias: "title",
             dataType: tableau.dataTypeEnum.string
-
+        }, {
+            id: "location",
+            dataType: tableau.dataTypeEnum.geometry
         }];
+
+        var tableSchema = {
+            id: "earthquakeFeed",
+            alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+            columns: cols
+        };
 
         schemaCallback([tableSchema]);
     };
 
     // Download the data
     myConnector.getData = function(table, doneCallback) {
-        $.getJSON("http://covidtracking.com/api/v1/us/current.csv", function(resp) {
-            var feat = resp,
+        $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
+            var feat = resp.features,
                 tableData = [];
 
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-					"date": feat[i].date,
-                    "state": feat[i].state
-			
+                    "id": feat[i].id,
+                    "mag": feat[i].properties.mag,
+                    "title": feat[i].properties.title,
+                    "location": feat[i].geometry
                 });
             }
 
